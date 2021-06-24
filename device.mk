1a32392127b9b63009d4fdef354e59e1de4f5869
sashascurtu <sasha.scurtu@gmail.com>
2021-06-24 20:20:22 +0000

    sweet: Build power stats HAL and label it
    * Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest
    * Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.sweet-libperfmgr
+    android.hardware.power-service.sweet-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
