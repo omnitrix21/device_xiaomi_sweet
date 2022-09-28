1b66421af8b73cb34ef7a0f7e6885db15ab296a4
sureshfizzy <sureshfizzy0503@gmail.com>
2022-09-28 12:35:40 +0530

    sweet: Import OnePlus Dolby
    [omnitrix21: rework on sepolicies and props]
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: ziasam <ziauddinsameer@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: sureshfizzy <sureshfizzy0503@gmail.com>
    Co-authored-by: Niranjan BR <niranjankannan2003@gmail.com>
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- properties.mk
+++ properties.mk
@@ -364,3 +364,13 @@ persist.debug.wfd.enable=1 \
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
 zygote.critical_window.minute=10
+
+# Dolby
+PRODUCT_VENDOR_PROPERTIES += \
+vendor.audio.dolby.ds2.enabled=false \
+vendor.audio.dolby.ds2.hardbypass=false \
+ro.vendor.dolby.dax.version=DAX3_3.6.0.12_r1 \
+ro.vendor.product.device.db=OP_DEVICE \
+ro.vendor.product.manufacturer.db=OP_PHONE \
+vendor.product.device=OP_PHONE \
+vendor.product.manufacturer=OPD
