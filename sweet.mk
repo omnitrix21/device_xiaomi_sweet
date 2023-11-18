83a28c390339227823b995da486a9e7390b4d686
omnitrix21 <okand7946@gmail.com>
2023-11-19 02:04:08 +0530

    sweet: project-zephyrus bringup
    signed-off-by: omnitrix21 <okand7946@gmail.com>

--- sweet.mk
+++ sweet.mk
@@ -11,13 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+CUSTOM_BUILD_TYPE := Official
+WITH_GMS := true
+USE_PIXEL_CHARGER_IMAGES := true
