c8f4924b50f590396df8c576a0e39243c2147600
omnitrix21 <okand7946@gmail.com>
2023-12-09 14:27:20 +0000

    sweet: track dependencies through vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone -b rising https://github.com/omnitrix21/device_xiaomi_sm6150-common device/xiaomi/sm6150-common
+git clone -b udc-release https://github.com/Project-Zephyrus/android_vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone -b udc-release https://github.com/Project-Zephyrus/android_vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
+git clone -b fourteen https://github.com/omnitrix21/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150 --depth=1
