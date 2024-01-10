280fa6b261f4fb5c4ce7b351b1f5b0ff746c9b23
omnitrix21 <okand7946@gmail.com>
2024-01-10 08:42:45 +0000

    sweet: change kernel defconfig path
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
 
 # Kernel
-TARGET_KERNEL_CONFIG := sweet_defconfig
+TARGET_KERNEL_CONFIG += vendor/sweet_perf_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
